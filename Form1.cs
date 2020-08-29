bd8cbdff36bed1e133b8fbb23a4a43ce4bba676c
ALI ZIA KHAN <azk443.443@gmail.com>
2020-08-29 18:28:57 +0500

    c sharp

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Image img = Image.FromFile("D:indiv.jpg");
+            pictureBox1.Image = img;
+            Image img1 = Image.FromFile("D:login.jpg");
+            pictureBox2.Image = img1;
+
+            textBox1.Text="Enter name";
+            textBox2.Text="passsword please";
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
